31b5bb1ba9e3348e712b807455a0d6f61407266f
Brayan Chinchilla <3017873950101@ingenieria.usac.edu.gt>
2020-10-17 16:01:18 -0600

    Se termino la vista del componente Item

--- src/components/Inventory/index.tsx
+++ src/components/Inventory/index.tsx
@@ -1,18 +1,68 @@
 import React from "react"
+import {useItemState} from "./state"
 
-export default function Item(){
+interface Card {
+    id: string,
+    name: string,
+    image: string,
+    chargeRate: number,
+    active: boolean,
+    availability: Array<number>
+}
+
+const dataQuemada: Card = {
+    id: "1",
+    name: "Google Play",
+    image: "https://media.karousell.com/media/photos/products/2020/5/21/rm50_goggle_play_gift_card_mal_1590040469_c1100b5a_progressive.jpg",
+    chargeRate: 1,
+    active: true,
+    availability: [
+      1,
+      2,
+      3,
+      4
+    ]
+  }
+
+export function Counter(){
     return (
-        <div className="card" style={{width: 500}}>
-            <img className="card-img-top" src="https://media.karousell.com/media/photos/products/2020/5/21/rm50_goggle_play_gift_card_mal_1590040469_c1100b5a_progressive.jpg"/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+        <div className="input-group mb-3">
+            <div className="input-group-prepend">
+                <button className="btn btn-primary" type="button">-</button>
+            </div>
+            <input type="number" className="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1"/>
+            <div className="input-group-append">
+                <button className="btn btn-primary" type="button">+</button>
             </div>
         </div>
     )
 }
 
-export function Inventory(){
+export function Item({card}: {card:Card}){
+    const state = useItemState();
 
+    return (
+        <div className="card" style={{width: 350}}>
+            <img className="card-img-top" src={card.image}/>
+            <div className="card-body">
+                <h5 className="card-title text-center">{card.name}</h5>
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <Counter/>
+                    </div>         
+                </div>
+                <div className="row">
+                        <button className="btn btn-block btn-primary">Comprar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default function Inventory(){
+    return (
+        <>
+        <Item card={dataQuemada}/>
+        </>
+    )
 }
