e9840d1dc8349927949b8513210003eb5c07d020
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-10-17 11:47:59 -0600

    Estructura inicial del componente

--- src/components/PagoTarjeta/index.tsx
+++ src/components/PagoTarjeta/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import usePagoTarjeta from './state'
 
 const PagoTarjeta: React.FC = () => (
   <div data-testid="pagotarjeta">
---
+++ src/components/PagoTarjeta/state.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const usePagoTarjeta: React.FC = () => (
+  <div data-testid="pagotarjeta">
+    Pago con tarjeta Component
+  </div>
+);
+
+export default usePagoTarjeta;
