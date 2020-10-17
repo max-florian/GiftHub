dec1351443c6dabc29af3a907e3b23a739f211a1
alexizzarevalo <dalexis.da@gmail.com>
2020-10-16 21:35:27 -0600

    Se agregó configuración de cors para conectar entre servidores.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 const routes = require('./routes');
 
 app.set('port', process.env.PORT || 4000);
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
