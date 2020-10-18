2a3e311a99083c31a5f5cb08203e13d4a5b72d0d
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-10-17 22:28:50 -0600

    Se implementa la estructura preliminar del form

--- src/components/PagoTarjeta/index.tsx
+++ src/components/PagoTarjeta/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import usePagoTarjeta from './state'
 
-const PagoTarjeta: React.FC = () => (
-  <div className="container" data-testid="pagotarjeta" style={{background:'green'}}>
+const PagoTarjeta: React.FC = () => {
+
+  return(<div className="container" data-testid="pagotarjeta" style={{background: 'green'}}>
     <div className="row">
-      Pago con tarjeta Component
+      <div className="col">
+        Número de tarjeta
+        Fecha de vencimiento
+        Mes --- Año
+        CVV ¿Qué es esto?
+        Datos de facturacion
+        Nombre en la tarjeta
+      </div>
     </div>
-  </div>
-);
+  </div>);
+}
 
 export default PagoTarjeta;
