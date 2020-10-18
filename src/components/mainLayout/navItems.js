0b26d2e775015b40b7e29e6332df01badf0d2fc8
Paola Casiano <p.casiano33@gmail.com>
2020-10-17 18:49:00 -0600

    sidebar

---
+++ src/components/mainLayout/navItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export const navItems = [
+    {
+        title:'Inicio',
+        path:'/',
+        icon:<i className="fas fa-home"></i>,
+        cname:'nav-text'
+    },
+    {
+        title:'Inventario',
+        path:'/inventario',
+        icon:<i className="fas fa-th"></i>,
+        cname:'nav-text'
+    }
+]
