67e2808f78207d7088c2156179cc99cd3d98f0e3
Darwin Arevalo <dalexis.da@gmail.com>
2020-10-23 00:35:56 -0600

    Se corrigio el middleware de auth porque daba un error de encabezados

--- backend/src/controllers/auth.middleware.js
+++ backend/src/controllers/auth.middleware.js
@@ -3,18 +3,20 @@ const response = require('./response');
 
 function verifyToken(req, res, next) {
     const secret = process.env.JWT_SECRET;
-    const token = req.headers.authorization.replace('Bearer ', '');
+    try {
+        const token = req.headers.authorization.replace('Bearer ', '');
 
-    if (token) {
-        verify(token, secret, (err, _) => {
-            if (err) {
-                return response(res, 401, false, 'Token no invalido', {});
-            } else {
-                next();
-            }
-        });
-    } else {
-        return response(res, 200, false, 'Token no proveido', {});
+        if (token) {
+            verify(token, secret, (err, _) => {
+                if (err) {
+                    return response(res, 401, false, 'Token invalido', {});
+                } else {
+                    return next();
+                }
+            });
+        } else return response(res, 400, false, 'Token no proveido', {});
+    } catch (error) {
+        return response(res, 400, false, 'Token no proveido', {});
     }
 };
 
