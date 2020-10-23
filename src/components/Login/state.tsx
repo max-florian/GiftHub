14fff0f91e3342cddff2d5f60d804d583cf44a81
Darwin Arevalo <dalexis.da@gmail.com>
2020-10-23 14:33:05 -0600

    Se redirige al perfil, guarda el userid en storage cuando inicia sesión

--- src/components/Login/state.tsx
+++ src/components/Login/state.tsx
@@ -1,11 +1,13 @@
 import { ChangeEvent, MouseEvent as EventMouse, useState } from "react";
 import utils from "../../utils/callApi";
-import { saveToken } from "../../utils/storage";
+import { saveToken, saveUserId } from "../../utils/storage";
+import { useHistory } from "react-router-dom";
 
 export default function useLoginState() {
     const [emailOrUsername, setEmailOrUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [errorMessage, setErrorMessage] = useState<string>('');
+    const history = useHistory();
 
     const login = (e: EventMouse<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
@@ -15,14 +17,18 @@ export default function useLoginState() {
             body: {
                 emailusername: emailOrUsername,
                 password: password
-            }
+            },
+            sendToken: false
         }).then((response) => {
+            console.log(response.data)
             if (!response.ok) setErrorMessage(response.message);
             else {
                 setErrorMessage('')
                 setEmailOrUsername('');
                 setPassword('');
-                saveToken(response.data.token)
+                saveToken(response.data.token);
+                saveUserId(response.data.userid);
+                history.replace('/profile');
             };
         });
     }
--- src/utils/callApi.tsx
+++ src/utils/callApi.tsx
@@ -2,14 +2,19 @@ import { getToken } from "./storage";
 
 const apiUrl = 'http://localhost:4000/api';
 
-async function callApi({ uri, method = 'GET', body = {} }: Props) {
+async function callApi({ uri, method = 'GET', body = {}, sendToken = true }: Props) {
+    let headers: any = {
+        "Content-Type": 'application/json',
+    }
+
+    if (sendToken) {
+        headers["Authorization"] = 'Bearer ' + getToken();
+    }
+
     const httpResponse = await fetch(apiUrl + uri, {
         method,
         body: method === 'GET' ? null : JSON.stringify(body),
-        headers: {
-            "Content-Type": 'application/json',
-            "Authorization": 'Bearer ' + getToken()
-        }
+        headers
     })
 
     const response = await httpResponse.json();
@@ -25,7 +30,8 @@ async function callApi({ uri, method = 'GET', body = {} }: Props) {
 type Props = {
     uri: string,
     method?: 'POST' | 'GET' | 'PUT' | 'DELETE',
-    body?: object
+    body?: object,
+    sendToken?: boolean,
 }
 
 export default {
