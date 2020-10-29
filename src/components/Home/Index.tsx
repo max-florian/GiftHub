e0f52c1e5d79a3548fa69cbce29e4faaf8c707c7
Darwin Arevalo <dalexis.da@gmail.com>
2020-10-29 16:48:27 -0600

    Se agrego mensaje de bienvenida en home

--- src/components/Home/Index.tsx
+++ src/components/Home/Index.tsx
@@ -5,9 +5,10 @@ export default function Home() {
     useSessionController({});
     return (
         <>
-            <div className='row'>
+            <div className='row' style={{ padding: 20 }}>
                 <div className='col-md-12'>
-                    <h1>HOME</h1>
+                    <h1>¡Bienvenido a Gifthub!</h1>
+                    <p>El lugar donde encuentras todas tus Gift Cards</p>
                 </div>
             </div>
         </>
